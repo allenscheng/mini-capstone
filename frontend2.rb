51214c378a55e008e1e6981ec1b50f7879c0a7f2
Allen Cheng <allencheng219@gmail.com>
2017-11-30 02:48:17 -0600

    add refactor frontend using class with methods

--- frontend2.rb
+++ frontend2.rb
@@ -6,6 +6,7 @@ class Frontend
     @base_url = "http://localhost:3000"
   end
   def show_menu
+    system "clear"
     puts "Welcome to the clothes app."
     puts "Please enter in an option."
     puts "1 - to print all the clothes"
@@ -24,24 +25,24 @@ class Frontend
     puts ""
     puts "[q] Quit"
   end
-  def show_all
+  def show_all_products
     response = Unirest.get("#{@base_url}/v1/clothes")
     clothes = response.body
     pp clothes
   end
-  def show_name
-    print "Please enter a name: "
+  def show_all_products_search
+    print "Please enter search terms: "
     input_name = gets.chomp 
     response = Unirest.get("#{@base_url}/v1/clothes", parameters: {name_search: input_name})
     clothes = response.body
     pp clothes
   end
-  def sort_price
+  def show_all_products_sorted_by_price
     response = Unirest.get("#{@base_url}/v1/clothes", parameters: {price_sort: true})
     clothes = response.body
     pp clothes 
   end
-  def create_item
+  def create_product
     params = {}
     print "Enter clothe type: "
     params["name"] = gets.chomp
@@ -61,7 +62,7 @@ class Frontend
     clothes = response.body
     pp clothes
   end
-  def individual_item_order_item
+  def show_one_product
     print "Enter clothes id to view: "
     clothes_id = gets.chomp
     response = Unirest.get("#{@base_url}/v1/clothes/#{clothes_id}")
@@ -78,85 +79,106 @@ class Frontend
       pp response.body
     end
   end
+  def update_product
+    print "Enter the id to update: "
+    clothes_id = gets.chomp
+    response = Unirest.get("#{@base_url}/v1/clothes/#{clothes_id}")
+    clothes = response.body
+    params = {}
+    print "Enter updated clothe type(#{clothes["name"]}): "
+    params["name"] = gets.chomp
+    print "Enter updated length(#{clothes["length"]}): "
+    params["length"] = gets.chomp
+    print "Enter updated color(#{clothes["color"]}): "
+    params["color"] = gets.chomp
+    print "Enter updated price(#{clothes["price"]}): "
+    params["price"] = gets.chomp
+    print "Enter updated image(#{clothes["image"]}): "
+    params["image"] = gets.chomp
+    print "Enter updated description(#{clothes["description"]}): "
+    params["description"] = gets.chomp
+    print "Enter updated active(#{clothes["active"]}): "
+    params["active"] = gets.chomp
+    params.delete_if { |_key, value| value.empty? }
+    response = Unirest.patch("#{@base_url}/v1/clothes/#{clothes_id}", parameters: params)
+    clothes = response.body
+    pp clothes
+  end
+  def delete_product
+    print "Enter a clothe id to remove: "
+    clothes_id = gets.chomp 
+    response = Unirest.delete("#{@base_url}/v1/clothes/#{clothes_id}")
+    pp response.body
+  end
+  def show_all_orders
+    response = Unirest.get("#{@base_url}/v1/orders")
+    pp response.body 
+  end
+  def signup
+    params = {}
+    print "Name: "
+    params[:name] = gets.chomp  
+    print "Email: "
+    params[:email] = gets.chomp  
+    print "Password: " 
+    params[:password] = gets.chomp
+    print "Password confirmation: "
+    params[:password_confirmation] = gets.chomp
+    response = Unirest.post("#{@base_url}/v1/users", parameters: params)
+    pp response.body 
+  end
+  def login
+    puts "Login to the app" 
+    params = {}
+    print "Email: "
+    params[:email] = gets.chomp
+    print "Password: " 
+    params[:password] = gets.chomp
+    response = Unirest.post("#{@base_url}/user_token", parameters: {auth: params})
+    pp response.body
+    # Save the JSON web token from the response
+    jwt = response.body["jwt"]
+    # Include the jwt in the headers of any future web requests
+    Unirest.default_header("Authorization", "Bearer #{jwt}")
+    pp response.body
+  end
+  def logout
+    jwt = ""
+    Unirest.clear_default_headers()
+    puts "Logged out successfully."
+  end
+  def quit
+    puts "Bye!!!!"
+    exit
+  end
   def run
     while true 
-      system "clear"
       show_menu
       user_choice = gets.chomp 
-
       if user_choice == "1"
-        show_all
+        show_all_products
       elsif user_choice == "1.1"
-        show_name
+        show_all_products_search
       elsif user_choice == "1.2"
-        sort_price
+        show_all_products_sorted_by_price
       elsif user_choice == "2"
-        create_item
+        create_product
       elsif user_choice == "3"
-        individual_item_order_item
+        show_one_product
       elsif user_choice == "4"
-        print "Enter the id to update: "
-        clothes_id = gets.chomp
-        response = Unirest.get("#{@base_url}/v1/clothes/#{clothes_id}")
-        clothes = response.body
-        params = {}
-        print "Enter updated clothe type(#{clothes["name"]}): "
-        params["name"] = gets.chomp
-        print "Enter updated length(#{clothes["length"]}): "
-        params["length"] = gets.chomp
-        print "Enter updated color(#{clothes["color"]}): "
-        params["color"] = gets.chomp
-        print "Enter updated price(#{clothes["price"]}): "
-        params["price"] = gets.chomp
-        print "Enter updated image(#{clothes["image"]}): "
-        params["image"] = gets.chomp
-        print "Enter updated description(#{clothes["description"]}): "
-        params["description"] = gets.chomp
-        print "Enter updated active(#{clothes["active"]}): "
-        params["active"] = gets.chomp
-        params.delete_if { |_key, value| value.empty? }
-        response = Unirest.patch("#{@base_url}/v1/clothes/#{clothes_id}", parameters: params)
-        clothes = response.body
-        pp clothes
+        update_product
       elsif user_choice == "5"
-        print "Enter a clothe id to remove: "
-        clothes_id = gets.chomp 
-        response = Unirest.delete("#{@base_url}/v1/clothes/#{clothes_id}")
-        pp response.body
+        delete_product
       elsif user_choice == "6"
-        response = Unirest.get("#{@base_url}/v1/orders")
-        pp response.body 
+        show_all_orders
       elsif user_choice == "signup"
-        params = {}
-        print "Name: "
-        params[:name] = gets.chomp  
-        print "Email: "
-        params[:email] = gets.chomp  
-        print "Password: " 
-        params[:password] = gets.chomp
-        print "Password confirmation: "
-        params[:password_confirmation] = gets.chomp
-        response = Unirest.post("#{@base_url}/v1/users", parameters: params)
-        pp response.body 
+        signup
       elsif user_choice == "login" 
-        puts "Login to the app" 
-        params = {}
-        print "Email: "
-        params[:email] = gets.chomp
-        print "Password: " 
-        params[:password] = gets.chomp
-        response = Unirest.post("#{@base_url}/user_token", parameters: {auth: params})
-        pp response.body
-        # Save the JSON web token from the response
-        jwt = response.body["jwt"]
-        # Include the jwt in the headers of any future web requests
-        Unirest.default_header("Authorization", "Bearer #{jwt}")
+        login
       elsif user_choice == "logout"
-        jwt = ""
-        Unirest.clear_default_headers()
+        logout
       elsif user_choice == "q"
-        puts "Bye!!!!"
-        break
+        quit
       end
       puts  
       puts "Press enter to continue."
